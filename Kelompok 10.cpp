cfd0392d39a96de063fc99884cd2a70dd56de143
Wil456789 <alwirahawian469@gmail.com>
2024-09-12 13:29:46 +0700

    Add files via upload

---
+++ Kelompok 10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+int i, n, jumlah;
+
+for (i=11; i>=1; i--){
+	
+	if (i%2 != 0){
+		cout << i << endl;
+	
+	jumlah = jumlah + i;
+	
+}
+}
+cout << "jumlah : " << jumlah << endl;
+}
